1f02aba8654bbf078614cc0eeb7fc658ef526bbc
bryanCasabuena <bryancasabuenac@gmail.com>
2024-08-22 18:04:22 +0800

    storing data into the database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from datetime import datetime
+
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,24 +11,31 @@ db = SQLAlchemy(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firts_name = db.Column(db.String(80))
+    first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    email_name = db.Column(db.String(200))    
+    email = db.Column(db.String(80))    
     date = db.Column(db.Date)
     occupation = db.Column(db.String(80))
-print()
+
+
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        first_name = request.form("first_name")
-        last_name = request.form("last_name")
-        email = request.form("email")
-        date = request.form("date")
-        occupation = request.form("occupation")
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        occupation = request.form["occupation"]
+            
+        form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+        flash("Your form was submitted successfully!", "success")  
         
     return render_template("index.html")
     
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-app.run(debug=True, port =5001)
+        app.run(debug=True, port =5001)
